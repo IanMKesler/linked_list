1bb5f451d107063c4239c1520c3dc01202ec55b4
Ian Kesler <ianmkesler@gmail.com>
2019-09-19 13:36:06 -0700

    Add size, head, tail and at(index) functionality

--- linked_list.rb
+++ linked_list.rb
@@ -1,6 +1,7 @@
 class LinkedList
     require_relative "node"
-    attr_accessor :name
+
+    attr_reader :head, :tail
 
     def initialize
         @head = nil
@@ -30,7 +31,26 @@ class LinkedList
         end
     end
 
-    
+    def size
+        count = 0
+        proc = Proc.new { |node| 
+            count+=1
+        }
+        traverse(proc) if @head
+        count
+    end
+
+    def at(index)
+        count = 0
+        proc = Proc.new { |node| 
+            count += 1
+            return node if count == index+1
+        }
+        traverse(proc) if @head
+        return false
+    end
+
+
 
     private
 
@@ -39,6 +59,11 @@ class LinkedList
         node_name = "@node_#{@number}"
         self.instance_variable_set(node_name,  Node.new(value))
         node_name
+    end
+
+    def traverse(proc, node = @head)
+        proc.call(node)
+        traverse(proc, node.next_node) if node.next_node      
     end
 
     
--- main.rb
+++ main.rb
@@ -4,4 +4,6 @@ require_relative "linked_list"
 list = LinkedList.new
 list.prepend(42)
 #list.prepend("The answer")
+puts list.size
+puts list.at(0).value
 gets
